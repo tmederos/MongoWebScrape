9e82e8b414c35a42ed2dde21f5006a67ee9e51dc
Trish Mederos <trishmederos@gmail.com>
2018-05-13 14:23:08 -0700

    update

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 // Dependencies
-var express = require("express");
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
-var exphbs = require("express-handlebars");
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var exphbs = require('express-handlebars');
 // Requiring our Note and Article models
-var Note = require("./models/Note.js");
-var Article = require("./models/Article.js");
-var router = require( "./routes/router");
+var Note = require('./models/Note.js');
+var Article = require('./models/Article.js');
+var router = require( './routes/router');
 
 var PORT = 3000;
 
 // Initialize Express
 var app = express();
 // Make public a static dir
-// Serve static content for the app from the "public" directory in the application directory.
-const path = require("path");
-app.use(express.static(path.join(__dirname, "public")));
+// Serve static content for the app from the 'public' directory in the application directory.
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Use body parser with our app
 app.use(bodyParser.urlencoded({
@@ -23,20 +23,20 @@ app.use(bodyParser.urlencoded({
 }));
 
 // set up database
-var db = process.env.MONGODB_URI || "mongodb://localhost/MongoHeadlines";
+var db = process.env.MONGODB_URI || 'mongodb://localhost/MongoHeadlines';
 mongoose.Promise = Promise;
 mongoose.connect(db, function(error) {
 	if (error) {
 		console.log(error);
 	} else {
-		console.log("mongoose connection is sucessful");
+		console.log('mongoose connection is sucessful');
 	}
 });
 
 
 //set engine and default for handlebars
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // Have every request go through router middlewar
 app.use(router);
@@ -46,5 +46,5 @@ var port = process.env.PORT || 3000;
 
 //setup listener
 app.listen(port, function() {
-  console.log("app running on port " + port);
+  console.log('app running on port ' + port);
 });
